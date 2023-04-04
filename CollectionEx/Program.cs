01691c49efd8dbd4f23906434872484f983d2d3c
Amit-Limbasiya-27 <amitlimbasiya27@gmail.com>
2023-04-04 11:24:35 +0530

    hii

---
+++ CollectionEx/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CollectionEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = { 12, 15, 23, 34, 45 };
+            //Console.WriteLine(Array.BinarySearch(arr, 25));
+            //var t = Tuple.Create( 1 );
+            var person = (PersonId: 1, FName: "Bill", LName: "Gates");
+            Console.WriteLine(person.PersonId); // returns 1
+            Console.WriteLine(person.FName); // returns "Bill"
+            Console.WriteLine(person.LName); // returns "Gates"
+
+        }
+    }
+}
